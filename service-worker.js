103acc1c57ffdcdd08e01baca3e8c4c6ada6ae4f
shankark1984 <46882965+shankark1984@users.noreply.github.com>
2024-09-24 02:43:26 +0530

    ok

--- service-worker.js
+++ service-worker.js
@@ -10,43 +10,39 @@ const urlsToCache = [
     '/manifest.json',
 ];
 
-// Install the service worker
-self.addEventListener('install', (event) => {
+// Install event
+self.addEventListener('install', event => {
     event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then((cache) => {
-          console.log('Opened cache');
-          return cache.addAll(urlsToCache);
-        })
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+            .then(() => self.skipWaiting()) // Activate the new service worker immediately
     );
-  });
-  
-  // Fetch event
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request)
-        .then((response) => {
-          // Cache hit - return the response from the cached version
-          if (response) {
-            return response;
-          }
-          return fetch(event.request);
-        })
-    );
-  });
-  
-  // Activate event
-  self.addEventListener('activate', (event) => {
+});
+
+// Activate event
+self.addEventListener('activate', event => {
     const cacheWhitelist = [CACHE_NAME];
+
     event.waitUntil(
-      caches.keys().then((cacheNames) => {
-        return Promise.all(
-          cacheNames.map((cacheName) => {
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+        .then(() => self.clients.claim()) // Ensure the new service worker takes control immediately
     );
-  });
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
